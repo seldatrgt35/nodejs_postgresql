b6050f0a245c7e31d7eaf888a7159876e282d313
selda <selda@aventurasoftware.com.tr>
2025-03-03 15:11:16 +0300

    database

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize({
     host: 'localhost',
     dialect: 'postgres',
-    password: 'selda2003',
+    password: '',
     database: 'test',
 });
 
